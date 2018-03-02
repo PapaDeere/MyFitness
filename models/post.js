ade1a0ad754a717f4921d8263216f871a8e6d0f9
William Campbell Crocker <williamcrocker1992@gmail.com>
2018-03-01 21:19:55 -0700

    prototype_post

---
+++ models/post.js
@@ -0,0 +1,22 @@
+module.exports = function(sequelize, DataTypes) {
+  var Post = sequelize.define("Post", {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      
+    },
+    LastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+
+
+  });
+  return Post;
+};
+
