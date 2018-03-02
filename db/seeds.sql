cd55481c55588cf56f9be103aac53387dd4968dd
William Campbell Crocker <williamcrocker1992@gmail.com>
2018-03-01 20:28:17 -0700

    fixedsql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,3 @@
+insert into contactUs (contacter_first_name, contacter_last_name, contacter_email, contacter_phone, contacter_message)
+	values ("Will", "Crocker", "Williamcrocker1992@gmail.com", 3294565, "help");
 
-
-insert into joinTheChallenger 
-	(challenger_first_name) values ("Will");
-insert into joinTheChallenger
-	(challenger_last_name) values ("Crocker");
-insert into joinTheChallenger
-	(challenger_email) values ("williamcrocker1992@gmail.com");
-insert into joinTheChallenger
-	(challenger_password) values ("test");
-insert into joinTheChallenger
-	(challenger_password_verify) values ("test");
-
-insert into contactUs
-	(contacter_first_name) values ("Papa");
-insert into contactUs
-	(contacter_last_name) values ("Deere");
-insert into contactUs
-	(contacter_email) values ("idunno@papadeere.com");
-insert into contactUs
-	(contacter_phone) values (6028729999);
-insert into contactUs
-	(contacter_message) values ("Help");
