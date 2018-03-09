e557f116e2629e19a77a9cac5683e8b2947333a5
Papa Deere <papadeere5150@gmail.com>
2018-03-08 20:45:32 -0700

    corrections

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ var bodyParser = require("body-parser");
 
 var app = express();
 
+app.use(express.static("assets"));
+
 var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 var db = require("./models");
+require("./routes/htmlRoutes")(app);
 require("./controllers/myFitness_controller.js")(app);
 
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
